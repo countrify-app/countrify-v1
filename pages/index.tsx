4afebd86ad3a929356d019f7a83b9c27abca542c
mrprst <prost.mathias@gmail.com>
2022-11-16 00:16:24 +0100

    recharts api + first tests + utils for UN Api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,42 @@
-import styles from '../styles/Home.module.css'
-import { Button } from '@countrify-app/countrify-components'
+import styles from "../styles/Home.module.css";
+import { Button, Logo } from "@countrify-app/countrify-components";
+import logo from "../public/logo.png";
+import { years, countries } from "../utils/data"
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 
-export default function Home() {
+export default function Home({ data }: any) {
+  let arr = [];
+  console.log(data.dataSets[0].series);
+  let employment = Object.values(data.dataSets[0].series)[0].observations;
+  Object.keys(employment).forEach(function (key: number, index: number) {
+    arr.push({name: years[key], uv: employment[key][0], pv: 100});
+  });
+  // Object.keys(employment).forEach(key => {
+  //   employment[key] = employment[key][0]
+  // });
+  console.log(countries)
+
   return (
     <div className={styles.container}>
-      <Button label='je-suis-un-petit-bouton'/>
+      <Button label="bouton" kind="red" />
+      <LineChart width={400} height={400} data={arr}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+      </LineChart>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const data = await fetch(
+    `https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.10/..SL_TLF_UEM.36.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD`,
+    {
+      headers: {
+        Accept: "text/json",
+      },
+    }
+  ).then((response) => response.json());
+  return { props: { data } };
 }
---
+++ utils/data.ts
@@ -0,0 +1,35 @@
+type Years = {
+  [key: number]: number
+}
+
+export const years: Years = {
+  0: 2000,
+  1: 2001,
+  2: 2002,
+  3: 2003,
+  4: 2004,
+  5: 2005,
+  6: 2006,
+  7: 2007,
+  8: 2008,
+  9: 2009,
+  10: 2010,
+  11: 2011,
+  12: 2012,
+  13: 2013,
+  14: 2014,
+  15: 2015,
+  16: 2016,
+  17: 2017,
+  18: 2018,
+  19: 2019,
+  20: 2020,
+}
+
+type Countries = {
+  [key: string]: number
+}
+
+export const countries: Countries = {
+  "Afghanistan": 4, "Albania": 8, "Algeria": 12, "American Samoa": 16, "Andorra": 20, "Angola": 24, "Anguila": 660, "Antigua and Barbuda": 28, "Argentina": 32, "Armenia": 51, "Aruba": 533, "Australia": 36, "Austria": 40, "Azerbaijan": 31, "Bahamas, The": 44, "Bahrain": 48, "Bangladesh": 50, "Barbados": 52, "Belarus": 112, "Belgium": 56, "Belgium-Luxembourg": 58, "Belize": 84, "Benin": 204, "Bermuda": 60, "Bhutan": 64, "Bolivia": 68, "Bosnia and Herzegovina": 70, "Botswana": 72, "Br. Antr. Terr": 80, "Brazil": 76, "British Indian Ocean Ter.": 86, "British Virgin Islands": 92, "Brunei": 96, "Bulgaria": 100, "Burkina Faso": 854, "Burundi": 108, "Cambodia": 116, "Cameroon": 120, "Canada": 124, "Cape Verde": 132, "Cayman Islands": 136, "Central African Republic": 140, "Chad": 148, "Chile": 152, "China": 156, "Christmas Island": 162, "Cocos (Keeling) Islands": 166, "Colombia": 170, "Comoros": 174, "Congo, Dem. Rep.": 180, "Congo, Rep.": 178, "Cook Islands": 184, "Costa Rica": 188, "Cote d'Ivoire": 384, "Croatia": 191, "Cuba": 192, "Cyprus": 196, "Czech Republic": 203, "Czechoslovakia": 200, "Denmark": 208, "Djibouti": 262, "Dominica": 212, "Dominican Republic": 214, "East Timor": 626, "Ecuador": 218, "Egypt, Arab Rep.": 818, "El Salvador": 222, "Equatorial Guinea": 226, "Eritrea": 232, "Estonia": 233, "Ethiopia(excludes Eritrea)": 231, "Ethiopia(includes Eritrea)": 230, "European Union": 918, "Faeroe Islands": 234, "Falkland Island": 238, "Fiji": 242, "Finland": 246, "Fm Panama Cz": 592, "Fm Rhod Nyas": 717, "Fm Tanganyik": 835, "Fm Vietnam Dr": 868, "Fm Vietnam Rp": 866, "Fm Zanz- Pemb": 836, "Fr.So.Ant.Tr": 260, "France": 250, "Free Zones": 838, "French Guiana": 254, "French Polynesia": 258, "Gabon": 266, "Gambia, The": 270, "Gaza Strip": 274, "Georgia": 268, "German Democratic Republic": 278, "Germany": 276, "Ghana": 288, "Gibraltar": 292, "Greece": 300, "Greenland": 304, "Grenada": 308, "Guadeloupe": 312, "Guam": 316, "Guatemala": 320, "Guinea": 324, "Guinea - Bissau": 624, "Guyana": 328, "Haiti": 332, "Holy See": 336, "Honduras": 340, "Hong Kong, China": 344, "Hungary": 348, "Iceland": 352, "India": 356, "Indonesia": 360, "Iran, Islamic Rep.": 364, "Iraq": 368, "Ireland": 372, "Israel": 376, "Italy": 380, "Jamaica": 388, "Japan": 392, "Jhonston Island": 396, "Jordan": 400, "Kazakhstan": 398, "Kenya": 404, "Kiribati": 296, "Korea, Dem.Rep.": 408, "Korea, Rep.": 410, "Kuwait": 414, "Kyrgyz Republic": 417, "Lao PDR": 418, "Latvia": 428, "Lebanon": 422, "Lesotho": 426, "Liberia": 430, "Libya": 434, "Liechtenstein": 438, "Lithuania": 440, "Luxembourg": 442, "Macao": 446, "Macedonia, FYR": 807, "Madagascar": 450, "Malawi": 454, "Malaysia": 458, "Maldives": 462, "Mali": 466, "Malta": 470, "Marshall Islands": 584, "Martinique": 474, "Mauritania": 478, "Mauritius": 480, "Mexico": 484, "Micronesia, Fed.Sts.": 583, "Midway Islands": 488, "Moldova": 498, "Monaco": 492, "Mongolia": 496, "Montserrat": 500, "Morocco": 504, "Mozambique": 508, "Myanmar": 104, "Namibia": 516, "Nauru": 520, "Nepal": 524, "Netherlands": 528, "Netherlands Antilles": 530, "Neutral Zone": 536, "New Caledonia": 540, "New Zealand": 554, "Nicaragua": 558, "Niger": 562, "Nigeria": 566, "Niue": 570, "Norfolk Island": 574, "Northern Mariana Islands": 580, "Norway": 578, "Oman": 512, "Pacific Islands": 582, "Pakistan": 586, "Palau": 585, "Panama": 591, "Papua New Guinea": 598, "Paraguay": 600, "Pen Malaysia": 459, "Peru": 604, "Philippines": 608, "Pitcairn": 612, "Poland": 616, "Portugal": 620, "Puerto Rico": 630, "Qatar": 634, "Reunion": 638, "Romania": 642, "Russian Federation": 643, "Rwanda": 646, "Ryukyu Is": 647, "Sabah": 461, "Saint Helena": 654, "Saint Kitts - Nevis - Anguilla - Aru": 658, "Saint Pierre and Miquelon": 666, "Samoa": 882, "San Marino": 674, "Sao Tome and Principe": 678, "Sarawak": 457, "Saudi Arabia": 682, "Senegal": 686, "Seychelles": 690, "Sierra Leone": 694, "SIKKIM": 698, "Singapore": 702, "Slovak Republic": 703, "Slovenia": 705, "Solomon Islands": 90, "Somalia": 706, "South Africa": 710, "Soviet Union": 810, "Spain": 724, "Special Categories": 839, "Sri Lanka": 144, "St.Kitts and Nevis": 659, "St.Lucia": 662, "St.Vincent and the Grenadines": 670, "Sudan": 736, "Suriname": 740, "Svalbard and Jan Mayen Is": 744, "Swaziland": 748, "Sweden": 752, "Switzerland": 756, "Syrian Arab Republic": 760, "Taiwan": 158, "Tajikistan": 762, "Tanzania": 834, "Thailand": 764, "Togo": 768, "Tokelau": 772, "Tonga": 776, "Trinidad and Tobago": 780, "Tunisia": 788, "Turkey": 792, "Turkmenistan": 795, "Turks and Caicos Isl.": 796, "Tuvalu": 798, "Uganda": 800, "Ukraine": 804, "United Arab Emirates": 784, "United Kingdom": 826, "United States": 840, "Unspecified": 898, "Uruguay": 858, "Us Msc.Pac.I": 849, "Uzbekistan": 860, "Vanuatu": 548, "Venezuela": 862, "Vietnam": 704, "Virgin Islands(U.S.)": 850, "Wake Island": 872, "Wallis and Futura Isl.": 876, "Western Sahara": 732, "World": 0, "Yemen Democratic": 720, "Yemen, Rep.": 887, "Yugoslavia": 891, "Yugoslavia, FR(Serbia / Montene": 890, "Zambia": 894, "Zimbabwe": 716
+}
